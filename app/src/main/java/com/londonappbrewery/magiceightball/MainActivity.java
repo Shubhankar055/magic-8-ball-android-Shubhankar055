ba3d0dd0277a4ae1024c802a0550f94cb59d3136
Shubhankar Shubham <subhankarsubham18@gmail.com>
2020-03-16 12:38:21 +0530

    Assignment Completed

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,12 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+        Button RollBtn = (Button) findViewById(R.id.button);
+        final ImageView ball  = (ImageView) findViewById(R.id.imageView);
+
+        final int[] Roll_Ary = {
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+
+        };
+
+
+        RollBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random randomNumberGenerator = new Random();
+
+                int Random_Number = randomNumberGenerator.nextInt(4);
+                ball.setImageResource(Roll_Ary[Random_Number]);
+
+                Log.d("Roll", "The Button has been pressed and the number is " + Random_Number);
+
+
+
+
+
+            }
+        });
     }
 }
